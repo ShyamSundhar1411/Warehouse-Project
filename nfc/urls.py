252b08add4f10722a98e119e34e4023b13741211
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-07-09 12:13:44 +0530

    Mark Attendance

--- nfc/urls.py
+++ nfc/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("",views.getRoutes,name = "get_attendance_routes"),
     path("get/users/all/",views.NFCUserAPIView.as_view(),name = "get_nfc_users"),
     path("get/meetings/all/",views.MeetingAPIView.as_view(),name = "get_meetings"),
+    path('mark/attendance/',views.mark_attendance,name = "mark_attedance"),
     path("get/attendances/all/",views.AttendanceAPIView.as_view(),name = "get_attendance")
 ]
--- nfc/views.py
+++ nfc/views.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from .models import *
 from .serializers import *
 from django.shortcuts import render
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework import status
 # Create your views here.
 
 @api_view((['GET']))
@@ -23,7 +25,30 @@ def getRoutes(request):
         '/mark/attendance/',
     ]
     return Response(routes)
+@api_view(['POST'])
+def mark_attendance(request):
+    data = request.data
+    meeting = Meeting.objects.get(id=data['meeting_id'])
+    user = NFCUser.objects.get(id=data['user_id'])
+    attendance = Attendance.objects.filter(meeting=meeting, user=user).first()
 
+    if attendance:
+        attendance.status = data['status']
+        formatted_date = datetime.strptime(data['date'], '%Y-%m-%d %H:%M:%S')
+        attendance.date = formatted_date
+        attendance.save()
+    else:
+        formatted_date = datetime.strptime(data['date'], '%Y-%m-%d %H:%M:%S')
+        attendance = Attendance.objects.create(
+            meeting=meeting,
+            user=user,
+            status=data['status'],
+            date=formatted_date
+        )
+
+    # Return a status code as response
+    return Response(status=status.HTTP_200_OK)
+        
 ## Class Based Views
 class NFCUserAPIView(generics.ListCreateAPIView):
     model = NFCUser
@@ -42,7 +67,6 @@ class MeetingAPIView(generics.ListCreateAPIView):
 class AttendanceAPIView(generics.ListCreateAPIView):
     model = Attendance
     serializer_class = AttendanceSerializer
-    
     def get_queryset(self):
         return Attendance.objects.all()
     
